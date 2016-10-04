5817d52b5effea5029496fe17b0ff861dea2c3f4
yveg <gambaly@gmail.com>
2016-10-04 17:02:35 +0200

    MVC DAO,Service,Controler 2

---
+++ src/main/java/streaming/dao/PersonneDAO.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package streaming.dao;
+
+import java.util.List;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+import streaming.entity.Personne;
+
+/**
+ *
+ * @author admin
+ */
+@Repository
+public class PersonneDAO {
+
+    @PersistenceContext
+    private EntityManager em;
+
+    public List<Personne> listepersonne() {
+        return em.createQuery("SELECT p FROM Personne p ORDER BY p.nom,p.prenom DESC").getResultList();
+    }
+
+    @Transactional //remplace em.getTransaction().begin(); avec em.getTransaction().commit();
+    public void ajouter(Personne p) {
+        em.persist(p);
+
+    }
+
+}
